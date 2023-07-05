187670e03265531e9db9a48480642c494cb33a60
SANG\phamq <phamquangsang661@gmail.com>
2023-07-05 13:13:01 +0700

    fix: loading overlay with error type

--- src/libs/components/overlay/loading-overlay.tsx
+++ src/libs/components/overlay/loading-overlay.tsx
@@ -10,15 +10,9 @@ export const LoadingOverlay = ({
 }: LoadingOverlay) => {
   return (
     <>
-      {/* {isLoading && <div className="fixed left-0 top-0 z-[9999] flex min-h-screen w-full items-center justify-center text-md font-bold text-primary-500">
-        <div className="flex flex-col gap-3 items-center">
-          <div className=" w-[80px] h-[80px] rounded-full border-4 border-primary-500"></div>
-          <span>{message}</span>
-        </div>
-        <div className="absolute z-[-999] h-full w-full bg-light opacity-70"></div>
-      </div>} */}
       {
         isLoading && <Dimmer active >
+          {/* @ts-expect-error Loding Component */}
           <Loader size="huge" className="!text-primary-500 !font-hiragino after:!border-[#EA6C00_transparent_transparent]">{message}</Loader>
         </Dimmer>
       }
